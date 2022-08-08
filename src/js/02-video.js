c00a9bd66b2e303cd4f520e72ce3e9cc79f12976
Oleksii Shevchenko <vazerius@gmail.com>
2022-08-08 17:37:59 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -9,8 +9,23 @@ function data({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(
-  JSON.parse(localStorage.getItem('videoplayer-current-time'))
-);
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    seconds = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 player.on('timeupdate', throttle(data, 1000));
+
+//   JSON.parse(localStorage.getItem('videoplayer-current-time'))
